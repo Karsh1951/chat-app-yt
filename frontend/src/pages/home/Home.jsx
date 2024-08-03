f20024438fb313eba01f0a934105b29f7452f703
Karsh1951 <giftonkarsh9851@gmail.com>
2024-08-03 19:45:54 +0300

    bug fix

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Sidebar from "../../components/sidebar/Sidebar";
 import MessageContainer from "../../components/sidebar/messages/MessageContainer";
-
+/** the home page */
 const Home = () => {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
