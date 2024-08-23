d5b8c661268270b95195541372b936ddf2f7a5be
Karsh1951 <giftonkarsh9851@gmail.com>
2024-08-23 10:30:59 +0300

    Getting messages with zusstand and assinging emoji to users

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -1,1 +1,12 @@
 // a global state
+import { create } from "zustand";
+
+const useConversation = create((set) => ({
+	selectedConversation: null,
+	setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
+	messages: [],
+	setMessages: (messages) => set({ messages }),
+}));
+
+export default useConversation;
+//similar to using the use state
